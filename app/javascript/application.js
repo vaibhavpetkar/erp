458f0a8b2430af63458ed00d5ff81a751607b0ca
Vaibhav Petkar <vaibhavprakashpetkar@gmail.com>
2024-09-12 15:36:35 +0530

    create nav bar only

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import jQuery from "jQuery"
-
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
+//import jQuery from "jQuery"
+// import { Application } from "stimulus"
+import { Application } from "@hotwired/stimulus"
 const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+
+// const context = require.context("./controllers", true, /\.js$/)
+// application.load(definitionsFromContext(context))
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+# pin "application"
+pin "application", preload: true
+
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
